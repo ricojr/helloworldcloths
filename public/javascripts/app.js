bd5723070458793e882a5a28067e4ea65deeece5
myle-truong <56028880+myle-truong@users.noreply.github.com>
2020-01-14 11:03:55 +0100

    update new html, css files

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -4,44 +4,44 @@ firebase.auth.Auth.Persistence.LOCAL;
 const loginForm = document.querySelector('#login-form');
 const btnLogin = document.querySelector('#btn-login');
 btnLogin.addEventListener('click', e => {
-  e.preventDefault();
+    e.preventDefault();
 
-  //get user info
-  const email = loginForm['login-email'].value;
-  const password = loginForm['login-password'].value;
+    //get user info
+    const email = loginForm['login-email'].value;
+    const password = loginForm['login-password'].value;
 
-  auth.signInWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred.user);
-  });
-
-  if (email != '' && password != '') {
-    const result = auth.signInWithEmailAndPassword(email, password);
-
-    result.catch(error => {
-      const errorCode = error.code;
-      const errorMessage = error.message;
-
-      console.log(errorCode);
-      console.log(errorMessage);
-
-      alert('Message: ' + errorMessage);
+    auth.signInWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred.user);
     });
-  } else {
-    alert('Please fill out all fields.');
-  }
+
+    if (email != '' && password != '') {
+        const result = auth.signInWithEmailAndPassword(email, password);
+
+        result.catch(error => {
+            const errorCode = error.code;
+            const errorMessage = error.message;
+
+            console.log(errorCode);
+            console.log(errorMessage);
+
+            alert('Message: ' + errorMessage);
+        });
+    } else {
+        alert('Please fill out all fields.');
+    }
 });
 
 //sign up
 const signupForm = document.querySelector('#signup-form');
 const btnSignup = document.querySelector('#btn-signup');
 btnSignup.addEventListener('click', e => {
-  e.preventDefault();
+    e.preventDefault();
 
-  //get user info
-  const email = signupForm['signup-email'].value;
-  const password = signupForm['signup-password'].value;
+    //get user info
+    const email = signupForm['signup-email'].value;
+    const password = signupForm['signup-password'].value;
 
-  auth.createUserWithEmailAndPassword(email, password).then(cred => {
-    console.log(cred);
-  });
+    auth.createUserWithEmailAndPassword(email, password).then(cred => {
+        console.log(cred);
+    });
 });
